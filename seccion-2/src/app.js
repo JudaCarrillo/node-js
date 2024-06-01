6dfe019b01a8b1dfb77bb9319a00645770f374a2
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-05-31 21:20:02 -0500

    callbacks and arrow functions

--- seccion-2/src/app.js
+++ seccion-2/src/app.js
@@ -1,6 +1,19 @@
 // import { emailTemplate } from "./js-foundation/01-template";
-const { emailTemplate } = require("./js-foundation/01-template");
-require("./js-foundation/02-destructuring");
+// const { emailTemplate } = require("./js-foundation/01-template");
+// require("./js-foundation/02-destructuring");
 
-console.log(emailTemplate);
+// console.log(emailTemplate);
 // console.log('Hola Mundo desde app.js');
+
+// const { getUserById } = require("./js-foundation/03-callbacks");
+const { getUserById } = require("./js-foundation/04-arrow");
+
+const id = 2;
+
+getUserById(id, (error, user) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(user);
+});
